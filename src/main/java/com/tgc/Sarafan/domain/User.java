e2b66e4c978c23fdcdb9d9d74d7baffbfbe8cdca
Alexandra <amaklov2002@gmail.com>
2022-09-11 17:30:55 +0400

    added security

---
+++ src/main/java/com/tgc/Sarafan/domain/User.java
@@ -0,0 +1,25 @@
+package com.tgc.Sarafan.domain;
+
+import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "usr")
+@Getter
+@Setter
+public class User {
+
+    @Id
+    private String id;
+
+    private String name;
+    private String userpic;
+    private String email;
+    private String gender;
+    private String locale;
+    private LocalDateTime lastVisit;
+}
