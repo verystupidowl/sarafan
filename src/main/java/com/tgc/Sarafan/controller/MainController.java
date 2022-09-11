d032d1ab7ce77971b51b64a35ac02e17d8dd0cc0
Alexandra <amaklov2002@gmail.com>
2022-09-12 00:36:16 +0400

    added components

--- src/main/java/com/tgc/Sarafan/controller/MainController.java
+++ src/main/java/com/tgc/Sarafan/controller/MainController.java
@@ -2,36 +2,42 @@ package com.tgc.Sarafan.controller;
 
 import com.tgc.Sarafan.domain.User;
 import com.tgc.Sarafan.repositories.MessageRepository;
-import org.springframework.beans.factory.annotation.Autowired;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.util.HashMap;
 
 @Controller
 @RequestMapping("/")
+@Slf4j
 public class MainController {
 
     private final MessageRepository messageRepository;
 
-    @Autowired
+    @Value("${spring.profile.active}")
+    private String profile;
+
     public MainController(MessageRepository messageRepository) {
         this.messageRepository = messageRepository;
     }
 
     @GetMapping
     public String main(Model model, @AuthenticationPrincipal User user) {
-        System.out.println(user);
-
         HashMap<Object, Object> data = new HashMap<>();
 
         data.put("profile", user);
         data.put("messages", messageRepository.findAll());
 
         model.addAttribute("frontendData", data);
+        model.addAttribute("isDevMode", "dev".equals(profile));
+
+        log.info(model.toString());
 
         return "index";
     }
---
+++ src/main/resources/static/js/pages/App.vue
@@ -0,0 +1,34 @@
+<template>
+  <div>
+    <div v-if="!profile">
+      Необходимо авторизироваться через <a href="/login">Google</a>
+    </div>
+    <div v-else>
+      <div>{{ profile.name }}&nbsp;<a href="/logout">Выйти</a></div>
+      <messages-list :messages="messages"/>
+    </div>
+  </div>
+</template>
+
+<script>
+import MessagesList from "../components/messages/MessagesList.vue";
+
+export default {
+  comments: {
+    MessagesList
+  },
+  data() {
+    return {
+      messages: frontendData.meggages,
+      profile: frontendData.profile
+    }
+  },
+  // created() {
+  //   this.$resource('/api/getData').get({}).then(result => result.json().then(data => ))
+  // }
+}
+</script>
+
+<style>
+
+</style>
