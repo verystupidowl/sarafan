948e151932244510612b256d8dd5c49e312de145
Alexandra <amaklov2002@gmail.com>
2022-09-11 12:58:41 +0400

    init commit

---
+++ src/test/java/com/tgc/Sarafan/SarafanApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tgc.Sarafan;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SarafanApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
