46918862f4e0336ff3175c6031f15f41b7c4ab90
Alexandra <amaklov2002@gmail.com>
2022-10-08 19:11:55 +0400

    added settings

--- src/main/resources/static/js/pages/Settings.vue
+++ src/main/resources/static/js/pages/Settings.vue
@@ -1,9 +1,17 @@
 <template>
-<v-container>
-  <h2>Settings</h2>
-  <v-layout>
-  </v-layout>
-</v-container>
+  <v-container>
+    <v-layout justify-space-around>
+      <v-flex :xs6="!$vuetify.breakpoint.xsOnly">
+        <h2>Settings</h2>
+        <v-layout row justify-space-between>
+          <v-layout column>
+            <v-flex>First setting</v-flex>
+            <v-flex>Second setting</v-flex>
+          </v-layout>
+        </v-layout>
+      </v-flex>
+    </v-layout>
+  </v-container>
 </template>
 
 <script>
