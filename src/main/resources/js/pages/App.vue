9dc15495ed5a487b86a816f3e745a9b48066dc8d
Alexey <amaklov2002@gmail.com>
2022-12-16 15:40:02 +0400

    styles edited

--- src/main/resources/js/pages/App.vue
+++ src/main/resources/js/pages/App.vue
@@ -1,7 +1,9 @@
 <template>
   <v-app>
     <v-toolbar app>
-      <v-toolbar-title v-if="profile" @click="showRecommendations">Sarafan</v-toolbar-title>
+      <v-toolbar-title v-if="profile" @click="showRecommendations" style="cursor: pointer">
+        Sarafan
+      </v-toolbar-title>
       <v-btn v-if="profile" flat :disabled="$route.path === '/'" @click="showMessages">
         Messages
       </v-btn>
